e639853fa0a293aeb4f364238f61518b2935ce62
Kamlesh Bhatt <kb55262555@gmail.com>
2023-11-10 11:18:41 +0530

    checking

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,7 @@
 export const aboutData = {
-    title: "Who I am",
-    description1: "My name's Kamlesh Bhatt. I'm a Full Stack Developer, based on Nainital Uttrakhand India.",
-    description2: "",
-    image: 2
-}
+  title: "Who I am",
+  description1:
+    "My name's Kamlesh Bhatt. I'm a Full Stack Developer, based on Nainital Uttrakhand India.",
+  description2: "Description 2 checking",
+  image: 2,
+};
