0e74c20a39abc1d24f8b06f82966aca07e28e868
Kamlesh Bhatt <kb55262555@gmail.com>
2024-01-24 21:22:41 +0530

    Added the 3D model

--- Portfolio2/src/components/Loader.jsx
+++ Portfolio2/src/components/Loader.jsx
@@ -1,7 +1,23 @@
 import { Html, useProgress } from "@react-three/drei";
+import { progress } from "framer-motion";
 
 const Loader = () => {
-  return <div>Loader</div>;
+  const { progrss } = useProgress();
+
+  return (
+    <Html>
+      <span className='canvas-load'></span>
+      <p
+        style={{
+          fontSize: 14,
+          color: `#f1f1f1`,
+          fontWeight: 800,
+          marginTop: 40,
+        }}>
+        {progress.toFixed(2)}%
+      </p>
+    </Html>
+  );
 };
 
 export default Loader;
