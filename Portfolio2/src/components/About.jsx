83c859728bc5c63d9bac0aa7faaafda58203a875
Kamlesh Bhatt <kb55262555@gmail.com>
2024-01-26 20:46:56 +0530

    Added cards

--- Portfolio2/src/components/About.jsx
+++ Portfolio2/src/components/About.jsx
@@ -6,6 +6,7 @@ import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { services } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
+import SectionWrapper from "../hoc/SectionWrapper";
 
 const ServiceCard = ({ index, title, icon }) => {
   return (
@@ -14,11 +15,11 @@ const ServiceCard = ({ index, title, icon }) => {
         variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
         className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'>
         <div
-          options={{
-            max: 45,
-            scale: 1,
-            speed: 450,
-          }}
+          // options={{
+          //   max: 45,
+          //   scale: 1,
+          //   speed: 450,
+          // }}
           className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px]
           flex justify-evenly items-center flex-col'>
           <img src={icon} alt={title} className='w-16 h-16 object-contain' />
@@ -63,4 +64,4 @@ const About = () => {
   );
 };
 
-export default About;
+export default SectionWrapper(About, "about");
