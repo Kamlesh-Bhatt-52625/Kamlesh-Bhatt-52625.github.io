019cfc0bfc5233933ff8f7388f6f0814ddfb1c02
Kamlesh Bhatt <kb55262555@gmail.com>
2024-01-26 11:01:09 +0530

    Started working on the About Section

--- Portfolio2/src/components/About.jsx
+++ Portfolio2/src/components/About.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
+import React from "react";
+import { Tilt } from "react-tilt";
 
 const About = () => {
-  return (
-    <div>About</div>
-  )
-}
+  return <div>About</div>;
+};
 
-export default About
+export default About;
