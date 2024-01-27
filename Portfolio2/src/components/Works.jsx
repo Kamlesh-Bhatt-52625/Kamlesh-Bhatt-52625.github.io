e62127ff6f99b4ababbd709f907afec96ab564b6
Kamlesh Bhatt <kb55262555@gmail.com>
2024-01-27 14:36:43 +0530

    Started working on contact section

--- Portfolio2/src/components/Works.jsx
+++ Portfolio2/src/components/Works.jsx
@@ -57,6 +57,19 @@ const ProjectCard = ({
             </div> */}
           </div>
         </div>
+
+        <div className='mt-5'>
+          <h3 className='text-white font-bold text-[24px]'>{name}</h3>
+          <p className='mt-2 text-secondary text-[14px]'>{description}</p>
+        </div>
+
+        <div className='mt-4 flex flex-wrap gap-2'>
+          {tags.map((tag) => (
+            <p key={tag.name} className={`tgext-[14px] ${tag.color}`}>
+              #{tag.name}
+            </p>
+          ))}
+        </div>
       </Tilt>
     </motion.div>
   );
