4685a50b63b375fdee159acc128b7625ef84aced
Kamlesh Bhatt <kb55262555@gmail.com>
2024-01-27 13:40:13 +0530

    Started Working on Projects

--- Portfolio2/src/components/Works.jsx
+++ Portfolio2/src/components/Works.jsx
@@ -1,4 +1,11 @@
-import React from 'react'
+import { Tilt } from "react-tilt"
+import { motion } from "framer-motion"
+
+import { styles } from "../styles"
+import { github } from "../assets"
+import SectionWrapper from "../hoc/SectionWrapper"
+import { projects } from "../constants"
+import { fadeIn, textVariant } from "../utils/motion"
 
 const Works = () => {
   return (
--- Portfolio2/src/constants/index.js
+++ Portfolio2/src/constants/index.js
@@ -14,7 +14,6 @@ import {
   mongodb,
   git,
   figma,
-  docker,
   carrent,
   jobit,
   tripguide,
